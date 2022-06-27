3de84ecad82cf701a2c0b25a86c777508dd51a71
karthik-d <karthikdesingu2000@gmail.com>
2022-06-28 03:55:32 +0530

    loss functor fixed

--- Experiments/src/prototypical/prototypical_loss.py
+++ Experiments/src/prototypical/prototypical_loss.py
@@ -34,62 +34,54 @@ class PrototypicalLoss(Module):
 
         return torch.pow(x - y, 2).sum(2)
 
+    @staticmethod
+    def prototypical_loss(input, target, n_support):
 
-def prototypical_loss(input, target, n_support):
+        """
+        Compute the barycentres by averaging the features of n_support
+        samples for each class in target, computes then the distances from each
+        samples' features to each one of the barycentres, computes the
+        log_probability for each n_query samples for each one of the current
+        classes, of appartaining to a class c, loss and accuracy are then computed
+        and returned
+        Args:
+        - input: the model output for a batch of samples
+        - target: ground truth for the above batch of samples
+        - n_support: number of samples to keep in account when computing
+        barycentres, for each one of the current classes
+        """
 
-    """
-    Compute the barycentres by averaging the features of n_support
-    samples for each class in target, computes then the distances from each
-    samples' features to each one of the barycentres, computes the
-    log_probability for each n_query samples for each one of the current
-    classes, of appartaining to a class c, loss and accuracy are then computed
-    and returned
-    Args:
-    - input: the model output for a batch of samples
-    - target: ground truth for the above batch of samples
-    - n_support: number of samples to keep in account when computing
-    barycentres, for each one of the current classes
-    """
+        target_cpu = target.to('cpu')
+        input_cpu = input.to('cpu')
 
-    target_cpu = target.to('cpu')
-    input_cpu = input.to('cpu')
+        def supp_idxs(c):
+            # FIXME when torch will support where as np
+            return target_cpu.eq(c).nonzero()[:n_support].squeeze(1)
 
-    def supp_idxs(c):
+        # FIXME when torch.unique will be available on cuda too
+        classes = torch.unique(target_cpu)
+        n_classes = len(classes)
         # FIXME when torch will support where as np
-        return target_cpu.eq(c).nonzero()[:n_support].squeeze(1)
+        # assuming n_query, n_target constants
+        n_query = target_cpu.eq(classes[0].item()).sum().item() - n_support
 
-    # FIXME when torch.unique will be available on cuda too
-    classes = torch.unique(target_cpu)
-    n_classes = len(classes)
-    # FIXME when torch will support where as np
-    # assuming n_query, n_target constants
-    n_query = target_cpu.eq(classes[0].item()).sum().item() - n_support
+        support_idxs = list(map(supp_idxs, classes))
 
-    support_idxs = list(map(supp_idxs, classes))
+        prototypes = torch.stack([input_cpu[idx_list].mean(0) for idx_list in support_idxs])
+        # FIXME when torch will support where as np
+        query_idxs = torch.stack(list(map(lambda c: target_cpu.eq(c).nonzero()[n_support:], classes))).view(-1)
 
-    prototypes = torch.stack([
-        input_cpu[idx_list].mean(0) 
-        for idx_list in support_idxs
-    ])
-    
-    query_idxs = torch.stack(
-        list(map(
-            lambda c: target_cpu.eq(c).nonzero()[n_support:], 
-            classes
-        ))
-    ).view(-1)
+        query_samples = input.to('cpu')[query_idxs]
+        dists = self.euclidean_dist(query_samples, prototypes)
 
-    query_samples = input.to('cpu')[query_idxs]
-    dists = PrototypicalLoss.euclidean_dist(query_samples, prototypes)
+        log_p_y = F.log_softmax(-dists, dim=1).view(n_classes, n_query, -1)
 
-    log_p_y = F.log_softmax(-dists, dim=1).view(n_classes, n_query, -1)
+        target_inds = torch.arange(0, n_classes)
+        target_inds = target_inds.view(n_classes, 1, 1)
+        target_inds = target_inds.expand(n_classes, n_query, 1).long()
 
-    target_inds = torch.arange(0, n_classes)
-    target_inds = target_inds.view(n_classes, 1, 1)
-    target_inds = target_inds.expand(n_classes, n_query, 1).long()
+        loss_val = -log_p_y.gather(2, target_inds).squeeze().view(-1).mean()
+        _, y_hat = log_p_y.max(2)
+        acc_val = y_hat.eq(target_inds.squeeze(2)).float().mean()
 
-    loss_val = -log_p_y.gather(2, target_inds).squeeze().view(-1).mean()
-    _, y_hat = log_p_y.max(2)
-    acc_val = y_hat.eq(target_inds.squeeze(2)).float().mean()
-
-    return loss_val,  acc_val
+        return loss_val,  acc_val
