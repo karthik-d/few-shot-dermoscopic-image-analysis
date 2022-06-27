d56d16b3ed7aa31304df20f32ba2ec096765f65d
karthik-d <karthikdesingu2000@gmail.com>
2022-06-27 09:40:59 +0530

    metaderm architecture defined

--- Experiments/src/architectures/metaderm.py
+++ Experiments/src/architectures/metaderm.py
@@ -9,9 +9,9 @@ def conv_block(in_channels, out_channels):
 
     return nn.Sequential(
         nn.Conv2d(in_channels, out_channels, 3, padding=1),
+        nn.MaxPool2d(2),
         nn.BatchNorm2d(out_channels),
         nn.ReLU(),
-        nn.MaxPool2d(2)
     )
 
 
@@ -21,16 +21,19 @@ class MetaDerm(nn.Module):
     Contructs an architecture as described in the reference paper (MetaDermDiagnosis-KMahajan)
 
     ```
+    (6 layers of)
     32 filters of size 3 x 3,
     and is followed by a 2 x 2 max pooling layer, batch nor-
     malization, and ReLU activation.
     ```
     """
     
-    def __init__(self, x_dim=1, hid_dim=64, z_dim=64):
+    def __init__(self, x_dim=3, hid_dim=32, z_dim=32):
         super(ProtoNet, self).__init__()
         self.encoder = nn.Sequential(
             conv_block(x_dim, hid_dim),
+            conv_block(hid_dim, hid_dim),
+            conv_block(hid_dim, hid_dim),
             conv_block(hid_dim, hid_dim),
             conv_block(hid_dim, hid_dim),
             conv_block(hid_dim, z_dim),
