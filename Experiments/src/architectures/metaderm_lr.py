dfcd218bf3696033cf50ae9490686dd8a9fdbd6c
karthik-d <karthikdesingu2000@gmail.com>
2022-07-17 14:11:08 +0530

    weight bias keys fixed

--- Experiments/src/architectures/metaderm_lr.py
+++ Experiments/src/architectures/metaderm_lr.py
@@ -27,6 +27,13 @@ class MetaDerm_LR(MetaDerm):
             self.avgpool = nn.AdaptiveAvgPool2d(1)
             self.classifier = nn.Linear(32, self.num_classes)
 
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+            elif isinstance(m, nn.BatchNorm2d):
+                nn.init.constant_(m.weight, 1)
+                nn.init.constant_(m.bias, 0)
+
     
     def forward(self, x):
         
