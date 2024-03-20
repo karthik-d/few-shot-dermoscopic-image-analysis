99dce931d1ed03d3a1883ec24aee85ec1bd604bb
karthik-d <karthikdesingu2000@gmail.com>
2024-03-21 00:08:50 +0530

    ph2 data wrapper

---
+++ experiments/src/data/PH2_Dataset.py
@@ -0,0 +1,143 @@
+from torch.utils.data import Dataset
+from torchvision import io 
+import os 
+import pandas as pd
+import torch
+
+from .config import config
+
+class PH2_Dataset(Dataset):
+
+    """
+    Extended custom `Dataset` class to interface
+    torch with the PH2 Dataset
+    """
+
+    class_id_map = dict(
+        MEL = 0,
+        NEV = 1
+        
+    )
+
+
+    def __init__(self, root, mode='train', img_ext='jpg', transform=None, target_transform=None):
+        
+        """
+        Initialize the Dataset
+        - root: Root directory containing all data files
+        - mode: Determines the CSV file to use (PH2_<mode>.csv)
+                Also determined the subdirectory for data split
+                Valid values: <subdirectory names within the data root>
+
+        NOTE: CSV must be present in the [ROOT]/data directory
+        """
+        #print("My root ="+root)
+        super(PH2_Dataset, self).__init__()
+
+        # Fetch CSV
+        self.csv_path = os.path.join(
+            config.csv_root_path,
+            f"PH2_{mode}.csv"
+        )
+        assert os.path.isfile(self.csv_path), f"CSV file was not found at {self.csv_path}"
+        
+        # Store CSV as Dataframe
+        self.csv_df = pd.read_csv(self.csv_path)
+        
+        # Ensure data path validity
+        self.img_base_path = root
+        assert os.path.isdir(self.img_base_path), f"Need valid data path as `root`. Got {root}"
+        self.img_concrete_path = os.path.join(
+            self.img_base_path,
+            mode 
+        )
+        assert os.path.isdir(self.img_concrete_path), f"Could not find valid data path at {self.img_concrete_path}"
+        
+        self.img_frmt_ext = img_ext
+        self.transform = transform
+        self.target_transform = target_transform
+        
+        self.num_classes = len(self.class_id_map)
+        self.class_names = list(self.class_id_map.keys())
+        # All `target` values of the dataset
+        self.labels = list(map(
+            lambda idx: self.get_sparse_label(self.csv_df, idx),
+            range(len(self.csv_df))
+        ))
+
+        print(f"Getting '{mode}' data from {self.img_concrete_path}")
+
+
+    def __len__(self):
+        return len(self.csv_df)
+
+
+    def __getitem__(self, idx):
+
+        """
+        Generates a single (img, label) pair
+
+        NOTE: Shuffling is taken care of by the DataLoader wrapper!
+        """
+        
+        if not isinstance(idx, int):
+            idx = idx.item()
+        
+        img_path = os.path.join(
+            self.img_concrete_path, 
+            "{0}.{1}".format(
+                self.csv_df.iloc[idx, 0], 
+                self.img_frmt_ext
+            )
+        )
+        img_label = self.get_sparse_label(self.csv_df, idx)
+        
+        # read data
+        try:
+            img_data = io.read_image(img_path).float()
+        except Exception as e:
+            print("Error when trying to read data file:", e)
+            return None 
+
+        # apply transforms
+        if self.transform is not None:
+            img_data = self.transform(img_data)
+        if self.target_transform is not None:
+            img_label = self.target_transform(img_label)
+        
+        # return data, label
+        return img_data, img_label
+
+
+    @staticmethod
+    def get_class_ids(class_names):
+        return [
+            PH2_Dataset.class_id_map.get(x)
+            for x in class_names
+        ]
+
+
+    @staticmethod 
+    def return_tensor(func):
+        
+        def wrapped(*args, **kwargs):
+            result = func(*args, **kwargs)
+            return torch.Tensor(result)
+
+        return wrapped
+
+    
+    @staticmethod
+    def get_sparse_label(csv_df, idx):
+        
+        """ 
+        Convert one-hot-encoded labels (from across the Dataframe columns)
+        to a single sparse label format
+        """
+
+        one_hot_label = csv_df.iloc[idx, 1:]
+        for classlabel, value in one_hot_label.items():
+            if value == 1:
+                return PH2_Dataset.class_id_map.get(classlabel)
+        return -1
+    
